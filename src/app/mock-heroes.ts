2fc8853cffad9b81543f9e3cd13bec1e7d4677b5
AshrefG <ashref.gammoudi@gmail.com>
2021-04-24 17:50:44 +0100

    init

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import {Hero} from './hero'
+
+export const HEROES: Hero[]= [
+  { id: 1, name: 'Ashref Gamoudi' },
+  { id: 2, name: 'b' },
+  { id: 3, name: 'c' },
+  { id: 4, name: 'd' },
+  { id: 5, name: 'e' },
+  { id: 6, name: 'f' },
+  { id: 7, name: 'g' }
+];
